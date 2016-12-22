2912eae4041bca4410c089cb6ed738c3b914c8af
Diana Connolly <diana.connolly@gmail.com>
2016-12-22 15:12:14 -0800

    corrections

--- gModule.py
+++ gModule.py
@@ -14,7 +14,7 @@ def getQuest(prevQuest):
 		[3, -1, "be friendly if someone bumped into you?"],
 		[4, -2, "go for a hike with a friend that's too broke to go to the club?"],
 		[4, -2, "give a dollar to a homeless person?"],
-		[5, -3,  "help a neighbor move?"],
+		[5, -3, "help a neighbor move?"],
 		[5, -3, "adopt an orphan kitten?"],
 		[-3, 1, "hide the cookies?"],
 		[-3, 1, "squash a spider?"],
@@ -25,7 +25,6 @@ def getQuest(prevQuest):
 	i = randint(0,11)
 	while i in prevQuest: # the probablility that we'll get a previously used quest is too high, so:
 		i = randint(0,11) # get a quest not used before in current game
-
 	prevQuest.append(i)
 	quest = [scenarios[i][0], scenarios[i][1], scenarios[i][2]]
 	return quest
@@ -45,7 +44,7 @@ def calcScore(action, quest, score):
 
 def outputResult(name, score): #scores can range from -25 .. 25
 	judgements = [
-		"Sntay away from me, you seem dangerous!!!!",
+		"Stay away from me, you seem dangerous!!!!",
 		"Maybe you should relax a little and not be such a turd.",
 		"I might hang around with you, on a good day!",
 		"You seem fun and normal!",
